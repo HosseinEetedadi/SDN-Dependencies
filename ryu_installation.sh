77980c899fd38176ace9e0ec974d141f8b2e5efa
Hossein Eetedadi <65198964+HosseinEetedadi@users.noreply.github.com>
2021-08-02 07:01:24 -0700

    Update ryu_installation.sh

--- ryu_installation.sh
+++ ryu_installation.sh
@@ -1,1 +1,9 @@
-
+sudo apt update
+sudo apt install pip
+pip install eventlet msgpack-python netaddr oslo.config routes six webob
+sudo apt install git
+git clone git://github.com/osrg/ryu.git
+pip install ryu
+sudo apt install python3-ryu
+cd ryu/
+ryu-manager --version
